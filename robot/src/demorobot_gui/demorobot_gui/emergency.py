a744e8e18931ea157716e2f7ec8d607fe378eafb
RoyLim <68123073+birds0416@users.noreply.github.com>
2024-03-04 16:50:06 +0900

    gui update

--- robot/src/demorobot_gui/demorobot_gui/emergency.py
+++ robot/src/demorobot_gui/demorobot_gui/emergency.py
@@ -29,8 +29,11 @@ class EmergencyMode(Node):
         self.ros2_worker = ROS2Worker(self)
         self.ros2_worker.start()
 
+        self.rgb_video_playing = False
+        self.depth_video_playing = False
+
         #region pose detect img
-        # self.pose_image = np.empty(shape=[1/])
+        self.pose_image = np.empty(shape=[1])
         # self.sub_pose_detect = self.create_subscription(Image, self.NAMESPACE + '/pose_detect/detect_img', self.pose_detect_callback, 10)
         self.sub_pose_detect = self.create_subscription(Image, '/device01/pose_detect/detect_img', self.pose_detect_callback, 10)
         # self.sub_depth_frame = self.create_subscription(Image, self.NAMESPACE + '/pose_detect/detect_img', self.depth_frame_callback, 10)
@@ -42,9 +45,6 @@ class EmergencyMode(Node):
         self.gui.detection_rgb_img_btn.clicked.connect(lambda: self.toggle_video_feed(1))
         self.gui.detection_depth_img_btn.clicked.connect(lambda: self.toggle_video_feed(2))
         self.gui.show()
-
-        self.rgb_video_playing = False
-        self.depth_video_playing = False
 
     def pose_detect_callback(self, msg):
         if self.rgb_video_playing:
@@ -59,46 +59,46 @@ class EmergencyMode(Node):
     def toggle_video_feed(self, num):
         # if self.pose_image == None:
         #     QMessageBox.warning(self.gui, 'Detect Image Error', "감지 이미지 접속 불가")
+        ''' rgb 이미지만 '''
+        # if self.rgb_video_playing:
+        #     self.gui.detection_rgb_img_btn.setText('감지 이미지 보기')
+        #     self.gui.rgb_img_label.hide()
+        # else:
+        #     self.gui.detection_rgb_img_btn.setText('Stop')
+        #     self.gui.rgb_img_label.show()
+        # self.rgb_video_playing = not self.rgb_video_playing
         
-        if num == 1:
-            # 아무것도 재생중이지 않을 때
-            if self.rgb_video_playing == False and self.depth_video_playing == False:
-                self.gui.rgb_img_label.show()
-                self.gui.detection_rgb_img_btn.setText('Stop')
+        ''' depth 이미지만 '''
+        # if self.depth_video_playing:
+        #     self.gui.detection_depth_img_btn.setText('Depth 이미지 보기')
+        #     self.gui.depth_img_label.hide()
+        # else:
+        #     self.gui.detection_depth_img_btn.setText('Stop')
+        #     self.gui.depth_img_label.show()
+        # self.depth_video_playing = not self.depth_video_playing
 
-            # rgb 재생중일때 끄기
-            elif self.rgb_video_playing and self.depth_video_playing == False:
+        ''' rgb, depth 이미지 '''
+        if num == 1:
+            if self.rgb_video_playing:
                 self.gui.rgb_img_label.hide()
                 self.gui.detection_rgb_img_btn.setText('감지 이미지 보기')
-            
-            # depth 재생중일때 끄고 rgb 재생
-            elif self.rgb_video_playing == False and self.depth_video_playing:
-                self.gui.depth_img_label.hide()
-                self.gui.detection_depth_img_btn.setText('Depth 이미지 보기')
+            else:
                 self.gui.rgb_img_label.show()
                 self.gui.detection_rgb_img_btn.setText('Stop')
-                self.depth_video_playing = not self.depth_video_playing
+                self.gui.depth_img_label.hide()
+                self.gui.detection_depth_img_btn.setText('Depth 이미지 보기' if self.depth_video_playing else 'Stop')
 
             self.rgb_video_playing = not self.rgb_video_playing
         
         elif num == 2:
-            # 아무것도 재생중이지 않을 때
-            if self.rgb_video_playing == False and self.depth_video_playing == False:
-                self.gui.depth_img_label.show()
-                self.gui.detection_depth_img_btn.setText('Stop')
-            
-            # depth 재생중일때 끄기
-            elif self.rgb_video_playing == False and self.depth_video_playing:
+            if self.depth_video_playing:
                 self.gui.depth_img_label.hide()
                 self.gui.detection_depth_img_btn.setText('Depth 이미지 보기')
-
-            # rgb 재생중일때 끄고 depth 재생
-            elif self.rgb_video_playing and self.depth_video_playing == False:
-                self.gui.rgb_img_label.hide()
-                self.gui.detection_rgb_img_btn.setText('감지 이미지 보기')
+            else:
                 self.gui.depth_img_label.show()
                 self.gui.detection_depth_img_btn.setText('Stop')
-                self.rgb_video_playing = not self.rgb_video_playing
+                self.gui.rgb_img_label.hide()
+                self.gui.detection_rgb_img_btn.setText('감지 이미지 보기' if self.rgb_video_playing else 'Stop')
             
             self.depth_video_playing = not self.depth_video_playing
 
